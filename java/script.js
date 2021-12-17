2a5748ad65bc35eed858fabfd925093ac57acaf2
Marcus M Nunkwe <90981152+MarcusMwaba@users.noreply.github.com>
2021-12-17 22:53:01 +0200

    Update script.js

--- java/script.js
+++ java/script.js
@@ -2,7 +2,7 @@ let weather = {
   apikey:"3d328acb66fe0da8928eaf9faa64ea03",
   fetchweather: function (city) {
       fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q="
+        "https://api.openweathermap.org/data/2.5/weather?q="
          + city 
         +"&units=metric&appid="
         +this.apikey
@@ -16,7 +16,7 @@ let weather = {
      const { temp, humidity } = data.main;
      const { speed } = data.wind;
      document.querySelector(".city").innerText = " weather in " + name;
-     document.querySelector(".icon").src="http://openweathermap.org/img/wn/" + icon +".png";
+     document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon +".png";
      document.querySelector(".description").innerText =  description;
      document.querySelector(".temp").innerText =  temp +"°C";
      document.querySelector(".humidity").innerText = "humidity :"+ humidity +"%";
